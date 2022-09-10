4de49bbb5462a1e54e1ec561ae58c705be4f28f2
HomeACcessoryKid <homeaccessorykid@gmail.com>
2022-09-10 15:12:27 +0200

    0.0.9 moved lcm-api to component
    and removed printf statements

--- main/lcm_api.h
+++
@@ -1,8 +0,0 @@
-/* (c) 2022 HomeAccessoryKid
- * LCM API
- * used with LCM4ESP32
- */
-#pragma once
-
-uint8_t lcm_read_count(); //read the restart count value from RTC
-void    lcm_temp_boot(); //restart with RTC value of temp_boot active
